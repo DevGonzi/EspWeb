76df7131dacd93f05d40c7d633e0d4a3ad6b7c52
Gonzi-Laptop <simjos3@gmail.com>
2019-11-11 11:38:47 +0100

    push

--- templates/test.php
+++ templates/test.php
@@ -2,4 +2,22 @@
 
 echo $_GET["sysvol"];
 
+
+$host = "85.214.153.77";
+$dbuser = "esp";
+$dbpass = "WqvYd3D5ptsF9tAWXONb";
+$dbname = "esp";
+$dbcon = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+
+
+
+
+$sql_insert = "INSERT INTO sysvol (sysvol) VALUES ('".$_GET["sysvol"]."')";
+if (mysqli_query($dbcon, $sql_insert)) {
+    echo "Eingetragen!";
+    mysqli_close($dbcon);
+} else {
+    echo "ERROR: " . mysqli_error($dbcon);
+}
+
 ?>
