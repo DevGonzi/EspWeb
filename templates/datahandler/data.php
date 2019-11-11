97cedf6241dd229c8edc3ec916296385c7a0e171
Gonzi-Laptop <simjos3@gmail.com>
2019-11-11 11:55:31 +0100

    change

--- templates/datahandler/data.php
+++ templates/datahandler/data.php
@@ -7,15 +7,15 @@ $dbcon = mysqli_connect($host, $dbuser, $dbpass, $dbname);
 
 // $sql_insert = "INSERT INTO data (temperature, humidity, heat_index) VALUES ('".$_GET["temperature"]."', '".$_GET["humidity"]."', '".$_GET["heat_index"]."')";
 
-
+$sql_insert = "INSERT INTO sysvol (sysvol) VALUES ('".$_GET["sysvol"]."')";
 $sql_sensor = "INSERT INTO data (temperatur, humidity, temperatur2, humidity2) VALUES ('" . $_GET["temperatur"] . "', '" . $_GET["humidity"] . "', '" . $_GET["temperatur2"] . "', '" . $_GET["humidity2"] . "')";
 // $sql_sensor2 = "INSERT INTO data (temperatur, humidity, sensor) VALUES ('" . $_GET["temperatur2"] . "', '" . $_GET["humidity2"] . "', '" . $_GET["sensor2"] . "')";
 
 // $sql_sensor1 = "INSERT INTO data (temperatur, humidity, sensor) VALUES ('" . $_GET["temperatur"] . "', '" . $_GET["humidity"] . "', '" . $_GET["sensor"] . "')";
 // $sql_sensor2 = "INSERT INTO data (temperatur, humidity, sensor) VALUES ('" . $_GET["temperatur2"] . "', '" . $_GET["humidity2"] . "', '" . $_GET["sensor2"] . "'
 
-// if (mysqli_query($dbcon, $sql_sensor1) && mysqli_query($dbcon, $sql_sensor2)) {
-if (mysqli_query($dbcon, $sql_sensor)) {
+if (mysqli_query($dbcon, $sql_sensor1) && mysqli_query($dbcon, $sqlinsert)) {
+// if (mysqli_query($dbcon, $sql_sensor)) {
     echo "Eingetragen!";
     mysqli_close($dbcon);
 } else {
@@ -26,7 +26,7 @@ if (mysqli_query($dbcon, $sql_sensor)) {
 
 
 
-
+/*
 $sql_insert = "INSERT INTO sysvol (sysvol) VALUES ('".$_GET["sysvol"]."')";
 if (mysqli_query($dbcon, $sql_insert)) {
     echo "Eingetragen!";
@@ -34,3 +34,7 @@ if (mysqli_query($dbcon, $sql_insert)) {
 } else {
     echo "ERROR: " . mysqli_error($dbcon);
 }
+*/
+
+
+?>
