55678da15d0efadbee68b18042fa9315580a737f
Gonzi-Laptop <simjos3@gmail.com>
2019-11-11 12:00:56 +0100

    push

--- templates/datahandler/data.php
+++ templates/datahandler/data.php
@@ -14,27 +14,12 @@ $sql_sensor = "INSERT INTO data (temperatur, humidity, temperatur2, humidity2) V
 // $sql_sensor1 = "INSERT INTO data (temperatur, humidity, sensor) VALUES ('" . $_GET["temperatur"] . "', '" . $_GET["humidity"] . "', '" . $_GET["sensor"] . "')";
 // $sql_sensor2 = "INSERT INTO data (temperatur, humidity, sensor) VALUES ('" . $_GET["temperatur2"] . "', '" . $_GET["humidity2"] . "', '" . $_GET["sensor2"] . "'
 
-if (mysqli_query($dbcon, $sql_sensor1) && mysqli_query($dbcon, $sql_insert)) {
+if (mysqli_query($dbcon, $sql_sensor) && mysqli_query($dbcon, $sql_insert)) {
 // if (mysqli_query($dbcon, $sql_sensor)) {
     echo "Eingetragen!";
     mysqli_close($dbcon);
 } else {
     echo "ERROR: " . mysqli_error($dbcon);
 }
-
-
-
-
-
-/*
-$sql_insert = "INSERT INTO sysvol (sysvol) VALUES ('".$_GET["sysvol"]."')";
-if (mysqli_query($dbcon, $sql_insert)) {
-    echo "Eingetragen!";
-    mysqli_close($dbcon);
-} else {
-    echo "ERROR: " . mysqli_error($dbcon);
-}
-*/
-
 
 ?>
